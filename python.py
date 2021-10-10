cfdf7e07682532b322537b86bc532051f6bf2f68
BlastuF01 <82941297+BlastuF01@users.noreply.github.com>
2021-10-10 08:43:25 +0300

    Add files via upload

---
+++ python.py
@@ -0,0 +1,46 @@
+import os
+from os import system, name 
+import sys
+import time
+import socket
+import threading
+import random
+from random import choice
+
+
+
+
+print("""
+                                              \033[00;0;35m╔╦╗┬ ┬┌─┐┌─┐┌┐┌  \033[0;34m╔╗╔┌─┐┌┬┐  
+                                               \033[00;0;35m║ └┬┘└─┐│ ││││  \033[0;34m║║║├┤  │   
+                                               \033[00;0;35m╩  ┴ └─┘└─┘┘└┘  \033[0;34m╝╚╝└─┘ ┴   
+""")
+
+
+
+answer = input("╚══════════➤  ")
+
+if answer == "clear":
+    os.system("cls")
+if answer == "CLEAR":
+    os.system("cls")
+
+
+print("""
+                                              \033[00;0;35m╔╦╗┬ ┬┌─┐┌─┐┌┐┌  \033[0;34m╔╗╔┌─┐┌┬┐  
+                                               \033[00;0;35m║ └┬┘└─┐│ ││││  \033[0;34m║║║├┤  │   
+                                               \033[00;0;35m╩  ┴ └─┘└─┘┘└┘  \033[0;34m╝╚╝└─┘ ┴   
+""")
+
+
+answer = input("╚══════════➤  ")
+
+def tcp():
+    os.system("cls")
+    os.system("python3 flood.py")
+
+
+
+input()
+
+
